28fcd07ac49f18580d61ba0ce964192b3c4eb74c
Gregory Jordan <gjordan@128technology.com>
2017-04-26 17:37:28 -0400

    Update distribution files.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { Tabs } from './components/Tabs';
 export { TabPane } from './components/TabPane';
 export { Duration } from './components/Duration';
 export { Provider } from './components/Provider';
+export { locales } from './components/Provider/locales';
