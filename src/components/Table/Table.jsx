3ce1d4ba9f78fc11757d5cb4fac31de2ea938f2e
Gregory Jordan <gjordan@128technology.com>
2017-05-04 11:39:44 -0400

    Add more styles

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import { Table as AntdTable } from 'antd';
 
@@ -8,8 +9,20 @@ import './Table.scss';
  * 
  * Please see the [Ant Design Table documentation](https://ant.design/components/table/)
  */
-const Table = ({ className = '', borderedTable = false, ...otherProps }) => (
-  <AntdTable {...otherProps} className={`${className} ${borderedTable ? 'ui-128__table--bordered' : ''} ui-128__table ui-128`} />
-);
+const Table = ({ className = '', borderedTable = false, expandedRowRender, ...otherProps }) => {
+  let newExpandedRowRender = null;
+
+  if (_.isFunction(expandedRowRender)) {
+    newExpandedRowRender = () => (<span className='ui-128 ui-128__table--nested'>{expandedRowRender()}</span>);
+  }
+
+  return (
+    <AntdTable
+      {...otherProps}
+      className={`${className} ${borderedTable ? 'ui-128__table--bordered' : ''} ui-128__table ui-128`}
+      expandedRowRender={newExpandedRowRender}
+    />
+  );
+};
 
 export default Table;
