35ce41abfb79559d824f39e79609fd2949167846
Chris Main <cmain@128technology.com>
2017-08-21 14:47:59 -0400

    Fix Menu ClassNames
    I95-14440

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Menu as AntdMenu } from 'antd';
 
 import './Menu.scss';
 
-function Menu({ className = '', ...otherProps }) {
+function Menu({ className, ...otherProps }) {
   return (
-    <AntdMenu {...otherProps} className={`${className} ui-128 ui-128__menu`} />
+    <AntdMenu {...otherProps} className={classNames(className, 'ui-128', 'ui-128__menu')} />
   );
 }
 
