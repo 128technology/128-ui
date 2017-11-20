8bbcad3b2e53f19997ceb0644c24930932f342fa
Andrew Ware <aware@128technology.com>
2017-11-20 11:26:07 -0500

    Fix table head filter, sort icon styles

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -6,12 +6,16 @@ import { Table as AntdTable } from 'antd';
 
 import './Table.scss';
 
+function TableCell({ children }) {
+  return <div className="ui-128 ui-128__table--cell">{children}</div>;
+}
+
 /**
  * 128 wrapper for the Ant Design `<Table />` component.
  *
  * Please see the [Ant Design Table documentation](https://ant.design/components/table/)
  */
-function Table({ className, borderedTable = false, expandedRowRender, onRowClick, ...otherProps }) {
+function Table({ borderedTable = false, className, columns, expandedRowRender, onRowClick, ...otherProps }) {
   const newExpandedRowRender = _.isFunction(expandedRowRender)
     ? (...args) => <span className="ui-128 ui-128__table--nested">{expandedRowRender(...args)}</span>
     : null;
@@ -23,6 +27,12 @@ function Table({ className, borderedTable = false, expandedRowRender, onRowClick
         'ui-128__table--bordered': borderedTable,
         'ui-128__table--selectable': onRowClick
       })}
+      columns={columns.map(column =>
+        _.assign({}, column, {
+          title: <TableCell>{column.title}</TableCell>,
+          render: (...args) => <TableCell>{_.isFunction(column.render) ? column.render(...args) : args[0]}</TableCell>
+        })
+      )}
       expandedRowRender={newExpandedRowRender}
       onRowClick={onRowClick}
     />
