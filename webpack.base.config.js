95eee053bcf8bc04ba9b8dfa590a78a0ab075cae
Gregory Jordan <gjordan@128technology.com>
2018-02-09 11:26:49 -0500

    Remove react-select from webpack

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -17,56 +17,57 @@ module.exports = {
     extensions: ['.jsx', '.js']
   },
   module: {
-    rules: [{
-      test: /\.(gif|png|jpg|svg|woff|ttf|eot|woff2)([\?]?.*)$/,
-      use: [{
-        loader: 'url-loader',
-        options: {
-          limit: 10000
-        }
-      }]
-    }, {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader'
-      ]
-    }, {
-      test: /\.jsx?$/,
-      include: [ APP_PATH ],
-      use: ['babel-loader']
-    }, {
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        {
-          loader: 'sass-loader',
-          options: {
-            data: '@import "./src/styles/common";',
-            sourceMap: true
+    rules: [
+      {
+        test: /\.(gif|png|jpg|svg|woff|ttf|eot|woff2)([\?]?.*)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
           }
-        }
-      ]
-    }]
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.jsx?$/,
+        include: [APP_PATH],
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              data: '@import "./src/styles/common";',
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
   },
-  externals: [{
-    react: {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react'
-    },
-    'react-select': {
-      commonjs2: 'react-select',
-      commonjs: 'react-select',
-      amd: 'react-select'
-    },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom'
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+      }
     }
-  }]
+  ]
 };
