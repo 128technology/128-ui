5037daced2275d3a10bb1f8d0739951ed0f8a3f7
Chris Main <cmain@128technology.com>
2018-08-03 15:34:59 -0400

    Add Match Modes to MatchText
    * Can now match using existing behavior, whole word match,
      case-sensitive match, and regex match.

--- src/components/MatchText/MatchText.jsx
+++ src/components/MatchText/MatchText.jsx
@@ -4,6 +4,10 @@ import _ from 'lodash';
 
 import './MatchText.scss';
 
+const CASE_SENSITIVE = 'CaseSensitive';
+const WHOLE_WORD = 'WholeWord';
+const REGEX = 'Regex';
+
 /**
  * Performs a case-insensitive search against an input string and highlights
  * the matches.
@@ -11,11 +15,27 @@ import './MatchText.scss';
  * Matches can be selected with css using `.ui-128__match-text--match`.
  * "Unmatched" text can also be selected using `.ui-128__match-text--unmatched`.
  */
-function MatchText({ text, searchString }) {
+function MatchText({ text, searchString, matchTypes }) {
+  const matchModesSet = new Set(matchTypes);
   const textString = _.toString(text);
 
   if (/\S+/.test(searchString)) {
-    const searchExp = new RegExp(_.escapeRegExp(searchString), 'ig');
+    let searchExp;
+    if (matchModesSet.has(REGEX)) {
+      try {
+        searchExp = new RegExp(searchString, 'g');
+      } catch (e) {
+        return <span className="ui-128 ui-128__match-text">{textString}</span>;
+      }
+    } else {
+      const regexOptions = matchModesSet.has(CASE_SENSITIVE) ? 'g' : 'ig';
+      const regexString = matchModesSet.has(WHOLE_WORD)
+        ? `\\b${_.escapeRegExp(searchString)}\\b`
+        : _.escapeRegExp(searchString);
+
+      searchExp = new RegExp(regexString, regexOptions);
+    }
+
     const unmatchedPieces = textString.split(searchExp);
     const matchedPieces = textString.match(searchExp);
 
@@ -50,7 +70,15 @@ MatchText.propTypes = {
   /**
    * The search string to search for.
    */
-  searchString: PropTypes.string.isRequired
+  searchString: PropTypes.string.isRequired,
+  /**
+   * The modes to match on, array of 'CaseSensitive', 'Regex', and 'WholeWord'.
+   */
+  matchTypes: PropTypes.arrayOf(PropTypes.oneOf([REGEX, CASE_SENSITIVE, WHOLE_WORD]))
+};
+
+MatchText.defaultProps = {
+  matchTypes: []
 };
 
 export default MatchText;
--- src/components/MatchText/__tests__/MatchText-test.js
+++ src/components/MatchText/__tests__/MatchText-test.js
@@ -4,20 +4,20 @@ import { expect } from 'chai';
 
 import MatchText from '../MatchText';
 
-describe('MatchText Component', function() {
-  it('should render without error', function() {
+describe('MatchText Component', () => {
+  it('should render without error', () => {
     const matchText = shallow(<MatchText text="" searchString="" />);
 
     expect(matchText).to.have.lengthOf(1);
   });
 
-  it('should accept numbers for text prop without error', function() {
+  it('should accept numbers for text prop without error', () => {
     const matchText = shallow(<MatchText text={2} searchString="2" />);
 
     expect(matchText).to.have.lengthOf(1);
   });
 
-  it('should render matched items', function() {
+  it('should render matched items', () => {
     const matchText = shallow(<MatchText text="A matchable search string..." searchString="search" />);
 
     const matched = matchText.find({ className: 'ui-128__match-text--matched' });
@@ -25,7 +25,15 @@ describe('MatchText Component', function() {
     expect(matched.text()).to.equal('search');
   });
 
-  it('should render unmatched items', function() {
+  it('should do a parial, case-insensitive match', () => {
+    const matchText = shallow(<MatchText text="A matchable search string..." searchString="Sea" />);
+
+    const matched = matchText.find({ className: 'ui-128__match-text--matched' });
+
+    expect(matched.text()).to.equal('sea');
+  });
+
+  it('should render unmatched items', () => {
     const matchText = shallow(<MatchText text="A matchable search string..." searchString="search" />);
 
     const unmatched = matchText.find({ className: 'ui-128__match-text--unmatched' });
@@ -33,7 +41,7 @@ describe('MatchText Component', function() {
     expect(unmatched).to.have.lengthOf(2);
   });
 
-  it("shouldn't match or unmatch an empty search string", function() {
+  it("shouldn't match or unmatch an empty search string", () => {
     const matchText = shallow(<MatchText text="A matchable search string..." searchString="" />);
 
     const matched = matchText.find({ className: 'ui-128__match-text--matched' });
@@ -41,5 +49,147 @@ describe('MatchText Component', function() {
 
     expect(matched).to.have.lengthOf(0);
     expect(unmatched).to.have.lengthOf(0);
+  });
+
+  describe('Regex Matching', () => {
+    it('should match on a regex', () => {
+      const matchText = shallow(
+        <MatchText text="A matchable search string..." searchString="m." matchTypes={['Regex']} />
+      );
+
+      const matched = matchText.find({ className: 'ui-128__match-text--matched' });
+      const unmatched = matchText.find({ className: 'ui-128__match-text--unmatched' });
+
+      expect(unmatched.at(0).text()).to.equal('A ');
+      expect(matched.at(0).text()).to.equal('ma');
+      expect(unmatched.at(1).text()).to.equal('tchable search string...');
+    });
+
+    it('should handle an invalid regex', () => {
+      const matchText = shallow(
+        <MatchText text="A matchable search string..." searchString="\" matchTypes={['Regex']} />
+      );
+
+      const component = matchText.find('.ui-128__match-text');
+
+      expect(component.text()).to.equal('A matchable search string...');
+    });
+
+    it('should handle no match', () => {
+      const matchText = shallow(
+        <MatchText text="A matchable search string..." searchString="m$" matchTypes={['Regex']} />
+      );
+
+      const matched = matchText.find({ className: 'ui-128__match-text--matched' });
+      const unmatched = matchText.find({ className: 'ui-128__match-text--unmatched' });
+
+      expect(unmatched).to.have.lengthOf(1);
+      expect(matched).to.have.lengthOf(0);
+    });
+  });
+
+  describe('Whole Word Matching', () => {
+    it('should match on a whole word', () => {
+      const matchText = shallow(
+        <MatchText text="A matchable search string..." searchString="search" matchTypes={['WholeWord']} />
+      );
+
+      const matched = matchText.find({ className: 'ui-128__match-text--matched' });
+      const unmatched = matchText.find({ className: 'ui-128__match-text--unmatched' });
+
+      expect(unmatched.at(0).text()).to.equal('A matchable ');
+      expect(matched.at(0).text()).to.equal('search');
+      expect(unmatched.at(1).text()).to.equal(' string...');
+    });
+
+    it('should not match if only a partial match', () => {
+      const matchText = shallow(
+        <MatchText text="A matchable search string..." searchString="searc" matchTypes={['WholeWord']} />
+      );
+
+      const matched = matchText.find({ className: 'ui-128__match-text--matched' });
+      const unmatched = matchText.find({ className: 'ui-128__match-text--unmatched' });
+
+      expect(unmatched).to.have.lengthOf(1);
+      expect(matched).to.have.lengthOf(0);
+    });
+  });
+
+  describe('Case Sensitive Matching', () => {
+    it('should match case sensitive', () => {
+      const matchText = shallow(
+        <MatchText text="A matchable search string..." searchString="sea" matchTypes={['CaseSensitive']} />
+      );
+
+      const matched = matchText.find({ className: 'ui-128__match-text--matched' });
+      const unmatched = matchText.find({ className: 'ui-128__match-text--unmatched' });
+
+      expect(unmatched.at(0).text()).to.equal('A matchable ');
+      expect(matched.at(0).text()).to.equal('sea');
+      expect(unmatched.at(1).text()).to.equal('rch string...');
+    });
+
+    it('should not match if case does not match', () => {
+      const matchText = shallow(
+        <MatchText text="A matchable search string..." searchString="Searc" matchTypes={['CaseSensitive']} />
+      );
+
+      const matched = matchText.find({ className: 'ui-128__match-text--matched' });
+      const unmatched = matchText.find({ className: 'ui-128__match-text--unmatched' });
+
+      expect(unmatched).to.have.lengthOf(1);
+      expect(matched).to.have.lengthOf(0);
+    });
+  });
+
+  describe('Case Sensitive, Whole Word Matching', () => {
+    it('should match case sensitive and whole word', () => {
+      const matchText = shallow(
+        <MatchText
+          text="A matchable search string..."
+          searchString="search"
+          matchTypes={['CaseSensitive', 'WholeWord']}
+        />
+      );
+
+      const matched = matchText.find({ className: 'ui-128__match-text--matched' });
+      const unmatched = matchText.find({ className: 'ui-128__match-text--unmatched' });
+
+      expect(unmatched.at(0).text()).to.equal('A matchable ');
+      expect(matched.at(0).text()).to.equal('search');
+      expect(unmatched.at(1).text()).to.equal(' string...');
+    });
+
+    it('should not match if case does not match', () => {
+      const matchText = shallow(
+        <MatchText
+          text="A matchable search string..."
+          searchString="Search"
+          matchTypes={['CaseSensitive', 'WholeWord']}
+        />
+      );
+
+      const matched = matchText.find({ className: 'ui-128__match-text--matched' });
+      const unmatched = matchText.find({ className: 'ui-128__match-text--unmatched' });
+
+      expect(unmatched).to.have.lengthOf(1);
+      expect(matched).to.have.lengthOf(0);
+    });
+
+    it('should not match if partial word', () => {
+      const matchText = shallow(
+        <MatchText
+          text="A matchable search string..."
+          searchString="earch"
+          matchTypes={['CaseSensitive', 'WholeWord']}
+        />
+      );
+
+      const matched = matchText.find({ className: 'ui-128__match-text--matched' });
+      const unmatched = matchText.find({ className: 'ui-128__match-text--unmatched' });
+
+      expect(unmatched).to.have.lengthOf(1);
+      expect(matched).to.have.lengthOf(0);
+    });
   });
 });
