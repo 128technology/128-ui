1bc536cc472b6e0f3ef3f7031eba90c2e9f0f3d4
Gregory Jordan <gjordan@128technology.com>
2019-01-09 11:38:50 -0500

    Fix splitbutton types

--- index.d.ts
+++ index.d.ts
@@ -192,21 +192,18 @@ declare function Spinner(props: ISpinnerProps): JSX.Element;
 
 export { Spinner };
 
-export enum Variant {
-  flat,
-  raised
-}
+export type Variant = 'flat' | 'raised';
 
 export interface ISplitButtonProps {
-  variant: Variant;
-  color: string;
+  variant?: Variant;
+  color?: string;
   popoverContent: React.ReactNode;
   defaultOnClick: (e: React.SyntheticEvent) => void;
   children: React.ReactNode;
-  dropdownIconClassName: string;
-  disabled: boolean;
-  dropdownDisabled: boolean;
-  closePopoverOnClick: boolean;
+  dropdownIconClassName?: string;
+  disabled?: boolean;
+  dropdownDisabled?: boolean;
+  closePopoverOnClick?: boolean;
 }
 
 declare class SplitButton extends React.Component<ISplitButtonProps, any> {}
