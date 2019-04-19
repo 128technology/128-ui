3d29239b97f936ea429411c9641d3a5a196e89c9
Benjamin Beshel <bbeshel@128technology.com>
2019-04-18 20:14:00 -0400

    Cleaned up code for WIP review

--- src/components/EnhancedTable/EnhancedTable.jsx
+++ src/components/EnhancedTable/EnhancedTable.jsx
@@ -3,18 +3,29 @@ import React from 'react';
 import Immutable from 'immutable';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import Table from '@material-ui/core/Table';
-import TablePagination from '@material-ui/core/TablePagination';
 import classNames from 'classnames';
 import naturalSort from 'javascript-natural-sort';
+import TableCell from '@material-ui/core/TableCell';
+import Checkbox from '@material-ui/core/Checkbox';
+import Radio from '@material-ui/core/Radio';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
+import { AutoSizer, MultiGrid } from 'react-virtualized';
 
+import { HORIZONTAL_BAR } from '../../constants/emptyCell';
 import Loading from '../Loading';
-import EnhancedTableHead from './EnhancedTableHead';
-import EnhancedTableBody from './EnhancedTableBody';
 import { defaultRowKey } from './enhancedTableUtil';
 
-const styles = {
+const DEFAULT_CELL_HEIGHT = 50;
+const DEFAULT_TABLE_HEIGHT = '400px';
+
+const styles = theme => ({
+  table: {
+    fontFamily: theme.typography.fontFamily,
+    outline: 'none',
+    '&:focus': { outline: 'none' }
+  },
   tableContainer: {
     position: 'relative'
   },
@@ -23,13 +34,44 @@ const styles = {
     flexGrow: '1'
   },
   tableContainerFullHeight: {
-    height: '100%',
+    height: '1000px',
     display: 'flex',
     flexDirection: 'column'
+  },
+  flexContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    boxSizing: 'border-box',
+    outline: 'none',
+    '&:focus': { outline: 'none' }
+  },
+  tableRow: {
+    cursor: 'pointer'
+  },
+  tableCell: {
+    fontWeight: '300'
+  },
+  tableCellFlex: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    boxSizing: 'border-box',
+    flexDirection: 'row',
+    outline: 'none',
+    '&:focus': { outline: 'none' }
+  },
+  centerFlex: {
+    justifyContent: 'center'
+  },
+  noClick: {
+    cursor: 'initial'
   }
-};
+});
 
-const sortComparator = (isNumeric, orderDirection) => (a, b) => {
+const sortComparator = (orderBy, orderDirection, isNumeric) => (first, second) => {
+  const a = _.get(first, orderBy);
+  const b = _.get(second, orderBy);
   if (_.isNil(a)) {
     return 1;
   }
@@ -49,65 +91,332 @@ class EnhancedTable extends React.Component {
     this.state = {
       orderBy: props.defaultOrderBy,
       orderDirection: props.defaultOrderDirection,
-      numericSort: false,
-      page: 0,
-      rowsPerPage: props.rowsPerPage || null
+      cols: null,
+      data: null,
+      scrollbars: null
     };
-
-    _.bindAll(this, ['handleRequestSort', 'handleRequestChangePage', 'handleRequestChangeRowsPerPage', 'sort']);
   }
 
-  handleRequestSort(dataIndex, isNumeric) {
+  componentDidUpdate = prevProps => {
+    if (prevProps.dataSource !== this.props.dataSource) {
+      this.setupData();
+    }
+    if (prevProps.columns !== this.props.columns) {
+      this.recomputeGridSize();
+    }
+  };
+
+  setupData = () => {
+    const { columns, dataSource, rowSelection } = this.props;
+
+    // TODO: force usage of plain js?
+    let cols = columns.toJS ? columns.toJS() : columns;
+    let data = dataSource.toJS ? dataSource.toJS() : dataSource;
+
+    if (rowSelection) {
+      cols.unshift({ width: 75, selectorType: rowSelection.get('selectorType'), key: 'checkbox', disableSort: true });
+    }
+
+    const headerRow = { grid__header: cols, name: 'grid__header' };
+    data.unshift(headerRow);
+
+    const colsWithFixedWidth = _.filter(cols, col => col.width);
+    const colsWithoutFixedWidth = _.differenceBy(cols, colsWithFixedWidth, 'label');
+    const fixedWidth = _.reduce(colsWithFixedWidth, (total, cur) => total + cur.width, 0);
+    const numColsFixedWidth = colsWithFixedWidth.length;
+
+    this.setState({
+      colsWithFixedWidth,
+      colsWithoutFixedWidth,
+      numColsFixedWidth,
+      totalFixedWidth: fixedWidth,
+      cols,
+      data
+    });
+  };
+
+  handleRequestSort = (dataKey, isNumeric) => {
     const otherDirection = {
       asc: 'desc',
       desc: 'asc'
     };
 
-    this.setState(({ orderBy, orderDirection }) => ({
-      orderBy: dataIndex,
-      numericSort: Boolean(isNumeric),
-      orderDirection: dataIndex !== orderBy ? 'desc' : otherDirection[orderDirection]
-    }));
-  }
+    this.setState(({ orderBy, orderDirection, cols, data }) => {
+      const newDirection = dataKey !== orderBy ? 'desc' : otherDirection[orderDirection];
+      const newData = _.cloneDeep(data).sort(sortComparator(dataKey, newDirection, isNumeric));
+      return {
+        orderBy: dataKey,
+        orderDirection: newDirection,
+        data: newData
+      };
+    });
+  };
 
-  handleRequestChangePage(event, page) {
-    this.setState({ page });
-  }
+  getRowProps = datum => {
+    const { rowProps } = this.props;
 
-  handleRequestChangeRowsPerPage(event) {
-    this.setState({ rowsPerPage: event.target.value });
-  }
-
-  sort(dataSource) {
-    const { orderDirection, orderBy, numericSort } = this.state;
-
-    if (!orderBy || !orderDirection) {
-      return dataSource;
+    if (_.isFunction(rowProps)) {
+      const calculatedRowProps = rowProps(datum);
+      return _.isPlainObject(calculatedRowProps) ? calculatedRowProps : {};
     }
 
-    return dataSource.sortBy(
-      x => (Immutable.List.isList(orderBy) ? x.getIn(orderBy) : x.get(orderBy)),
-      sortComparator(numericSort, orderDirection)
-    );
-  }
+    if (_.isObject(rowProps)) {
+      return rowProps;
+    }
 
-  validateColumnKeys() {
-    const { columns } = this.props;
+    return {};
+  };
 
-    const missingKeys = columns
-      .filter(x => !x.has('key'))
-      .map(x => x.get('title'))
-      .toSet();
+  validateColumnKeys(columns) {
+    const missingKeys = _.map(_.filter(_.uniqBy(columns, 'key'), x => !_.has(x, 'key')), x => x.label);
 
-    if (missingKeys.size > 0) {
+    if (missingKeys.length > 0) {
       throw `The key property is required and must be unique for each column configured. Affected columns ${missingKeys.join(
         ','
       )}`;
     }
   }
 
+  getSelectedRowCount = () => {
+    const { rowSelection, rowKey } = this.props;
+    const { data } = this.state;
+
+    const rowKeyFn = _.isFunction(rowKey) ? rowKey : defaultRowKey;
+
+    if (rowSelection && _.isFunction(rowSelection.get('rowIsSelected'))) {
+      return _.filter(data, x => rowSelection.get('rowIsSelected')(x, rowKeyFn(x))).length;
+    }
+
+    return 0;
+  };
+
+  noRowsRenderer = () => {
+    const { noDataText, rowHeight, classes } = this.props;
+
+    return (
+      <TableCell
+        style={{ height: `${rowHeight || DEFAULT_CELL_HEIGHT}px` }}
+        component="div"
+        variant="body"
+        className={classNames(classes.tableCellFlex, classes.centerFlex)}
+      >
+        {noDataText}
+      </TableCell>
+    );
+  };
+
+  getRowKeys = data => {
+    const { rowKey } = this.props;
+    return _.isFunction(rowKey) ? _.map(data, rowKey) : [];
+  };
+
+  _cellRenderer = cellRenderData => {
+    const { columnIndex, key: cellKey, rowIndex, style } = cellRenderData;
+    const { classes, rowRenderOptions, rowSelection, rowKey } = this.props;
+    const { cols, data, orderBy, orderDirection } = this.state;
+    const colData = cols[columnIndex];
+    const dataKey = colData.dataKey;
+    const rowData = data[rowIndex];
+    const cellData = _.get(data[rowIndex], dataKey);
+
+    const rowKeyFn = _.isFunction(rowKey) ? rowKey : defaultRowKey;
+    const customRenderer = colData.render;
+    const placeholder = colData.numeric ? 0 : HORIZONTAL_BAR;
+    const content = _.isNil(cellData) || cellData === '' ? placeholder : cellData;
+
+    // RENDER HEADER ROW
+
+    if (rowIndex === 0) {
+      const { disableSort, dataKey, label } = colData;
+
+      if (rowSelection && columnIndex === 0) {
+        if (rowSelection.get('selectorType') === 'radio') {
+          return <div key={cellKey} />;
+        }
+        const nonHeaderData = data.slice(1);
+        const dataLength = nonHeaderData.length;
+        const allRowSelectFn = _.isFunction(rowSelection.get('onSelectAll'))
+          ? rowSelection.get('onSelectAll')
+          : () => {};
+
+        const onChangeHandler = _.partialRight(allRowSelectFn, nonHeaderData, this.getRowKeys(nonHeaderData));
+
+        const selectedRowCount = this.getSelectedRowCount();
+
+        return (
+          <TableCell
+            key={cellKey}
+            style={style}
+            component="div"
+            variant="body"
+            padding="checkbox"
+            className={classNames(classes.tableCellFlex, colData.className)}
+          >
+            <Checkbox
+              indeterminate={selectedRowCount > 0 && selectedRowCount < dataLength}
+              checked={selectedRowCount === dataLength && dataLength > 0}
+              onChange={onChangeHandler}
+              disabled={dataLength === 0}
+            />
+          </TableCell>
+        );
+      }
+
+      return (
+        <TableCell
+          key={cellKey}
+          style={style}
+          component="div"
+          variant="head"
+          className={classNames(classes.tableCellFlex, colData.className)}
+          onClick={disableSort ? _.noop : _.partial(this.handleRequestSort, colData.dataKey, colData.numeric)}
+        >
+          {!disableSort && (
+            <TableSortLabel active={orderBy === dataKey} direction={_.toLower(orderDirection)}>
+              {label}
+            </TableSortLabel>
+          )}
+          {disableSort && label}
+        </TableCell>
+      );
+    }
+
+    const rowSelectFn =
+      rowSelection && _.isFunction(rowSelection.get('onSelect')) ? rowSelection.get('onSelect') : () => {};
+    const onChangeHandler = _.partialRight(rowSelectFn, rowData, rowKeyFn(rowData));
+
+    // RENDER SELECTION BUTTON COLUMNS
+    if (rowSelection && columnIndex === 0) {
+      if (rowSelection.get('selectorType') === 'radio') {
+        return (
+          <TableCell
+            key={cellKey}
+            style={style}
+            component="div"
+            variant="body"
+            padding="checkbox"
+            className={classNames(classes.tableCellFlex, colData.className)}
+          >
+            <Radio checked={rowSelection.get('rowIsSelected')(rowData, rowKeyFn(rowData))} onChange={onChangeHandler} />
+          </TableCell>
+        );
+      }
+      return (
+        <TableCell
+          key={cellKey}
+          style={style}
+          component="div"
+          variant="body"
+          padding="checkbox"
+          className={classNames(classes.tableCellFlex, colData.className)}
+        >
+          <Checkbox
+            checked={rowSelection.get('rowIsSelected')(rowData, rowKeyFn(rowData))}
+            onChange={onChangeHandler}
+          />
+        </TableCell>
+      );
+    }
+
+    const calcRowProps = this.getRowProps(rowData);
+
+    // ANY OTHER CELL
+    return (
+      <TableCell
+        {...calcRowProps}
+        style={style}
+        key={cellKey}
+        component="div"
+        variant="body"
+        className={classNames(classes.tableCellFlex, colData.className)}
+      >
+        {_.isFunction(customRenderer) ? customRenderer(cellData, rowData, rowRenderOptions) : content}
+      </TableCell>
+    );
+  };
+
+  _getColumnWidth = ({ index }, gridWidth) => {
+    const { columnMinWidth } = this.props;
+    const { cols, scrollbars, colsWithoutFixedWidth, totalFixedWidth: fixedWidth, numColsFixedWidth } = this.state;
+
+    const remainingWidth = gridWidth - fixedWidth;
+    const colLength = cols.length;
+    const isVerticalScrollbarVisible = scrollbars && scrollbars.vertical;
+    const isLastColumn = index === colLength - 1;
+    const lastColWithoutFixedWidth = _.last(colsWithoutFixedWidth);
+    const isLastColWithoutFixedWidth = _.isEqual(lastColWithoutFixedWidth, cols[index]);
+    const unusedSpace = remainingWidth > 0 && numColsFixedWidth === colLength;
+    const numColsNoFixedWidth = colLength - numColsFixedWidth;
+
+    let padding =
+      isVerticalScrollbarVisible && (isLastColWithoutFixedWidth || (isLastColumn && unusedSpace)) ? scrollbars.size : 0;
+    if (cols[index].width) {
+      if (unusedSpace) {
+        return remainingWidth + cols[index].width - padding;
+      }
+      return cols[index].width;
+    }
+
+    if (remainingWidth <= 0) {
+      return columnMinWidth;
+    }
+
+    const adjustedWidth = numColsNoFixedWidth > 0 ? remainingWidth / numColsNoFixedWidth : columnMinWidth;
+
+    const finalWidth = adjustedWidth - padding < columnMinWidth ? columnMinWidth : adjustedWidth - padding;
+    return _.floor(finalWidth);
+  };
+
+  getTableHeight = () => {
+    const { rowHeight, height, maxHeight } = this.props;
+    const { data, scrollbars } = this.state;
+
+    if (height) {
+      return height;
+    }
+
+    if (!data) {
+      return DEFAULT_TABLE_HEIGHT;
+    }
+
+    // TODO: fix this? data.length checks for 1 because we insert a dummy row for the column header
+    const numVisibleRenderedRows = data.length === 1 ? 2 : data.length;
+
+    const padding = scrollbars && scrollbars.horizontal ? scrollbars.size : 0;
+    let calcHeight = rowHeight ? rowHeight * numVisibleRenderedRows : DEFAULT_CELL_HEIGHT * numVisibleRenderedRows;
+    calcHeight += padding;
+    if (calcHeight > maxHeight) {
+      calcHeight = maxHeight;
+    }
+
+    return `${calcHeight}px`;
+  };
+
+  recomputeGridSize = () => {
+    if (this.ref) {
+      this.ref.recomputeGridSize();
+    }
+  };
+
+  setRef = ref => {
+    if (!this.ref) {
+      this.ref = ref;
+    }
+  };
+
+  scrollbarPresenceChange = scrollbars => {
+    const refreshGridSize = this.state.scrollbars ? _.noop : this.recomputeGridSize;
+
+    this.setState(
+      {
+        scrollbars
+      },
+      refreshGridSize
+    );
+  };
+
   render() {
-    const { orderBy, orderDirection, page, rowsPerPage } = this.state;
+    const { orderBy, orderDirection, isNumeric, cols, data } = this.state;
 
     const {
       columns,
@@ -117,53 +426,55 @@ class EnhancedTable extends React.Component {
       className,
       loading,
       classes,
+      height,
+      maxHeight,
       rowSelection,
+      rowHeight,
+      headerHeight,
+      rowClassName,
+      tableClassName,
       fullHeight,
       noDataText,
-      rowRenderOptions
+      rowRenderOptions,
+      columnMinWidth,
+      columnMaxWidth
     } = this.props;
 
-    this.validateColumnKeys();
+    if (!loading && !data) {
+      this.setupData();
+      this.validateColumnKeys(cols);
+    }
 
     return (
-      <div className={classNames(classes.tableContainer, { [classes.tableContainerFullHeight]: fullHeight })}>
+      <Paper style={{ height: this.getTableHeight(), width: '100%', boxShadow: 'none' }}>
         {loading && <Loading />}
-        <div className={classes.tableScrollArea}>
-          <Table className={className}>
-            <EnhancedTableHead
-              columns={columns}
-              onRequestSort={this.handleRequestSort}
-              orderBy={orderBy}
-              orderDirection={orderDirection}
-              dataSource={dataSource}
-              rowSelection={rowSelection}
-              rowKey={rowKey}
-            />
-            <EnhancedTableBody
-              columns={columns}
-              dataSource={this.sort(dataSource)}
-              rowKey={rowKey}
-              page={page}
-              rowsPerPage={rowsPerPage}
-              rowSelection={rowSelection}
-              rowProps={rowProps}
-              noDataText={noDataText}
-              rowRenderOptions={rowRenderOptions}
-            />
-          </Table>
-        </div>
-        {rowsPerPage && (
-          <TablePagination
-            rowsPerPage={rowsPerPage}
-            count={dataSource.size}
-            page={page}
-            onChangePage={this.handleRequestChangePage}
-            onChangeRowsPerPage={this.handleRequestChangeRowsPerPage}
-            rowsPerPageOptions={[5, 10, 15, 25]}
-            component="div"
-          />
+        {data && (
+          <AutoSizer onResize={_.debounce(this.recomputeGridSize, 200)}>
+            {({ height, width }) => (
+              <MultiGrid
+                ref={this.setRef}
+                data={data}
+                fixedRowCount={1}
+                fixedColumnCount={rowSelection && data.length - 1 > 0 ? 1 : 0}
+                cellRenderer={this._cellRenderer}
+                columnWidth={_.partial(this._getColumnWidth, _, width)}
+                columnCount={cols.length}
+                enableFixedColumnScroll
+                enableFixedRowScroll
+                noContentRenderer={this.noRowsRenderer}
+                width={width}
+                height={height}
+                onScrollbarPresenceChange={this.scrollbarPresenceChange}
+                rowHeight={rowHeight || DEFAULT_CELL_HEIGHT}
+                rowCount={data.length}
+                className={classNames(classes.table, tableClassName)}
+                hideTopRightGridScrollbar
+                hideBottomLeftGridScrollbar
+              />
+            )}
+          </AutoSizer>
         )}
-      </div>
+      </Paper>
     );
   }
 }
--- src/components/EnhancedTable/EnhancedTable.md
+++ src/components/EnhancedTable/EnhancedTable.md
@@ -1,23 +1,62 @@
 Example:
 
     const Immutable = require('immutable');
+    const { RowSelection } = require('./enhancedTableUtil');
 
-    const columns = Immutable.fromJS([{
-      title: 'Name',
-      key: 'name',
-      dataIndex: 'name'
-    }, {
-      title: 'Healthy',
-      key: 'health',
-      dataIndex: 'healthy'
-    }, {
-      title: 'Calories',
-      key: 'cals',
-      numeric: true,
-      dataIndex: 'cals' 
-    }]);
+    const columns = Immutable.fromJS([
+      {
+        label: 'Name',
+        key: 'name',
+        dataKey: 'name'
+      },
+      {
+        label: 'Policy',
+        key: 'policyName',
+        dataKey: 'policyName'
+      },
+      {
+        label: 'Type',
+        key: 'type'
+      }
+    ]);
 
     const data = Immutable.fromJS([{
+      name: 'Burger',
+    }, {
+      name: 'Carrot',
+      policyName: 'Yes',
+      type: 'fat'
+    }, {
+      name: 'Cookie',
+      policyName: 'No',
+      type: 'fat'
+    }]);
+
+    <EnhancedTable dataSource={[]} columns={columns} rowSelection={            new RowSelection({
+              selectorType: 'radio'
+            })} />
+
+Example:
+
+    const Immutable = require('immutable');
+
+    const columns = Immutable.fromJS([{
+      label: 'Name',
+      key: 'name',
+      dataKey: 'name',
+      flexGrow: 1
+    }, {
+      label: 'Healthy',
+      key: 'health',
+      dataKey: 'healthy',
+    }, {
+      label: 'Calories',
+      key: 'cals',
+      numeric: true,
+      dataKey: 'cals',
+    }]);
+
+    const d = Immutable.fromJS([{
       name: 'Burger',
       cals: 1400,
       healthy: 'Kinda'
@@ -27,12 +66,12 @@ Example:
       healthy: 'Yes'
     }, {
       name: 'Cookie',
-      cals: 220,
       healthy: 'No'
     }]);
 
-    <EnhancedTable dataSource={data} columns={columns} />
+    const data = d.flatMap(i => [i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i]);
 
+    <EnhancedTable maxHeight={150} dataSource={data} columns={columns} columnMinWidth={230} />
 
 Example:
 
@@ -40,18 +79,18 @@ Example:
     const { RowSelection } = require('./enhancedTableUtil');
 
     const columns = Immutable.fromJS([{
-      title: 'Name',
+      label: 'Name',
       key: 'name',
-      dataIndex: 'name'
+      dataKey: 'name',
+      flexGrow: 1
     }, {
-      title: 'Healthy',
+      label: 'Healthy',
       key: 'health',
-      dataIndex: 'healthy'
+      dataKey: 'healthy',
     }]);
 
     const data = Immutable.fromJS([{
       name: 'Burger',
-      healthy: 'Kinda'
     }, {
       name: 'Carrot',
       healthy: 'Yes'
@@ -61,3 +100,180 @@ Example:
     }]);
 
     <EnhancedTable dataSource={data} columns={columns} rowSelection={new RowSelection()} />
+
+Example:
+
+    const Immutable = require('immutable');
+
+
+    const columns = Immutable.fromJS([{
+      label: 'Device Interface',
+      flexGrow: 1,
+      dataKey: 'name',
+      key: 'name'
+    },
+    {
+      label: 'Network Interfaces',
+      flexGrow: 1,
+      dataKey: 'totalCount',
+      key: 'numNetworkInterfaces',
+      numeric: true
+    },
+    {
+      label: 'Type',
+      flexGrow: 1,
+      dataKey: 'type',
+      key: 'type'
+    },
+    {
+      label: 'Forwarding',
+      flexGrow: 1,
+      dataKey: 'forwarding',
+      key: 'forwarding',
+      render: (cellData) => (cellData ? 'Yes' : 'No')
+    },
+    {
+      label: 'MAC Address',
+      flexGrow: 1,
+      dataKey: 'state.macAddress',
+      cellDataGetter: ({ rowData }) => {
+        console.log('MACADDRESS', rowData);
+        return rowData.state.macAddress;
+      },
+      key: 'mac'
+    },
+    {
+      label: 'Admin Status',
+      flexGrow: 1,
+      dataKey: 'adminStatus',
+      cellDataGetter: ({ rowData }) => {
+        return rowData.state.adminStatus;
+      },
+      key: 'adminStatus',
+    },
+    {
+      label: 'Operational Status',
+      flexGrow: 1,
+      dataKey: 'operationalStatus',
+      cellDataGetter: ({ rowData }) => {
+        return rowData.state.operationalStatus;
+      },
+      key: 'operationalStatus'
+    },
+    {
+      label: 'Redundancy Status',
+      flexGrow: 1,
+      dataKey: 'redundancyStatus',
+      cellDataGetter: ({ rowData }) => {
+        return rowData.state.redundancyStatus;
+      },
+      key: 'redundancyStatus',
+    },
+    {
+      label: (
+        <React.Fragment>
+          <span key="text">{'Shared MAC Address'} </span>
+        </React.Fragment>
+      ),
+      dataKey: 'sharedPhysAddress',
+      key: 'sharedPhysAddress'
+    },
+    {
+      label: 'Avg. B/W (Mbps)',
+      flexGrow: 1,
+      dataKey: 'averageBandwidth',
+      key: 'averageBandwidth',
+      numeric: true,
+    }]);
+
+
+    const data = Immutable.fromJS([
+    {
+        "networkInterfaces": {
+            "totalCount": 1,
+        },
+        "name": "10-newname",
+        "state": {
+            "adminStatus": "ADMIN_UP",
+            "operationalStatus": "OPER_UP",
+            "redundancyStatus": "NON_REDUNDANT",
+            "macAddress": "fa:16:3e:13:eb:28",
+        },
+        "averageBandwidth": null,
+        "_id": "authority/router:NYC/node:nyc/device-interface:10-newname",
+        "type": "ethernet",
+        "sharedPhysAddress": null,
+        "forwarding": true
+    },
+    {
+        "networkInterfaces": {
+            "totalCount": 1,
+        },
+        "name": "11",
+        "state": {
+            "adminStatus": "ADMIN_UP",
+            "operationalStatus": "OPER_UP",
+            "redundancyStatus": "NON_REDUNDANT",
+            "macAddress": "fa:16:3e:a7:db:3a",
+        },
+        "averageBandwidth": 1320.200557103064,
+        "_id": "authority/router:NYC/node:nyc/device-interface:11",
+        "type": "ethernet",
+        "sharedPhysAddress": null,
+        "forwarding": true
+    },
+    {
+        "networkInterfaces": {
+            "totalCount": 1,
+        },
+        "name": "management",
+        "state": {
+            "adminStatus": null,
+            "operationalStatus": null,
+            "redundancyStatus": null,
+            "macAddress": null,
+        },
+        "averageBandwidth": null,
+        "_id": "authority/router:NYC/node:nyc/device-interface:management",
+        "type": "ethernet",
+        "sharedPhysAddress": null,
+        "forwarding": false
+    },
+    {
+        "networkInterfaces": {
+            "totalCount": 1,
+        },
+        "name": "sfc-ipsec",
+        "state": {
+            "adminStatus": "ADMIN_UP",
+            "operationalStatus": "OPER_UNKNOWN",
+            "redundancyStatus": "NON_REDUNDANT",
+            "macAddress": "da:7a:a3:6c:02:20",
+        },
+        "averageBandwidth": null,
+        "_id": "authority/router:NYC/node:nyc/device-interface:sfc-ipsec",
+        "type": "host",
+        "sharedPhysAddress": null,
+        "forwarding": true
+    },
+    {
+        "networkInterfaces": {
+            "totalCount": 1,
+        },
+        "name": "zscaler-0",
+        "state": {
+            "adminStatus": "ADMIN_UP",
+            "operationalStatus": "OPER_UNKNOWN",
+            "redundancyStatus": "NON_REDUNDANT",
+            "macAddress": "26:1f:fb:6f:d5:3a",
+        },
+        "averageBandwidth": null,
+        "_id": "authority/router:NYC/node:nyc/device-interface:zscaler-0",
+        "type": "host",
+        "sharedPhysAddress": null,
+        "forwarding": true
+    }]);
+
+
+    <EnhancedTable dataSource={data} columns={columns} columnMinWidth={200} />
+
