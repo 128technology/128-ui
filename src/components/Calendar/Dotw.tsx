a23243b204bcf68a672709a35d9d8cca10979ce1
Benjamin Beshel <bbeshel@128technology.com>
2020-11-11 13:59:49 -0500

    Updated the time picker so that the visual Day of the Week will now respect the browser's locale in regards to the first day of the week. This prevents a bug where any other 'first day of week' besides Sunday would cause the calendar dates to be off-by-one from the days of the week listed at the top (because the calendar dates update their position to match the 'first day of week' but the DotW component would not)

--- src/components/Calendar/Dotw.tsx
+++ src/components/Calendar/Dotw.tsx
@@ -1,14 +1,19 @@
-import * as _ from 'lodash';
-import * as React from 'react';
-import * as PropTypes from 'prop-types';
-import * as moment from 'moment';
-import { withStyles, Theme, createStyles, WithStyles } from '@material-ui/core/styles';
+import * as _ from "lodash";
+import * as React from "react";
+import * as PropTypes from "prop-types";
+import * as moment from "moment";
+import {
+  withStyles,
+  Theme,
+  createStyles,
+  WithStyles
+} from "@material-ui/core/styles";
 
 const styles = ({ typography, palette, spacing }: Theme) =>
   createStyles({
     dotwContainer: {
-      display: 'flex',
-      alignItems: 'center',
+      display: "flex",
+      alignItems: "center",
       color: palette.text.hint,
       fontFamily: typography.caption.fontFamily,
       fontSize: typography.caption.fontSize,
@@ -17,7 +22,7 @@ const styles = ({ typography, palette, spacing }: Theme) =>
     dotw: {
       width: 36,
       padding: `${spacing(1)}px 0`,
-      textAlign: 'center'
+      textAlign: "center"
     }
   });
 
@@ -25,24 +30,27 @@ export interface IProps extends WithStyles<typeof styles> {
   format?: string;
 }
 
-export const Dotw: React.FunctionComponent<IProps> = ({ format, classes }) => (
-  <div className={classes.dotwContainer}>
-    {_.range(0, 7).map(d => (
-      <div className={classes.dotw} key={d}>
-        {moment()
-          .day(d)
-          .format(format)}
-      </div>
-    ))}
-  </div>
-);
+export const Dotw: React.FunctionComponent<IProps> = ({ format, classes }) => {
+  const firstDayOfWeek = moment.localeData().firstDayOfWeek();
+  return (
+    <div className={classes.dotwContainer}>
+      {_.range(firstDayOfWeek, firstDayOfWeek + 7).map(d => (
+        <div className={classes.dotw} key={d}>
+          {moment()
+            .day(d % 7)
+            .format(format)}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 Dotw.propTypes = {
   format: PropTypes.string
 };
 
 Dotw.defaultProps = {
-  format: 'dd'
+  format: "dd"
 };
 
 export default withStyles(styles)(Dotw);
