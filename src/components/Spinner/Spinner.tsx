d44e120bee5938b361a21f06c4265b41c8ca7c1f
Dillon Buchanan <dbuchanan@128technology.com>
2021-01-07 17:05:31 -0500

    Update Spinner Icon

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,55 +1,57 @@
-import * as React from 'react';
-import * as PropTypes from 'prop-types';
-import * as classNames from 'classnames';
-import { styled } from '@material-ui/core/styles';
+import * as React from "react";
+import * as PropTypes from "prop-types";
+import * as classNames from "classnames";
+import { styled } from "@material-ui/core/styles";
 
-import { Logo } from '../Icons';
-import './Spinner.scss';
+import { Router } from "../Icons";
+import "./Spinner.scss";
 
 export interface IProps {
-  size?: 'small' | 'medium' | 'large' | 'huge';
+  size?: "small" | "medium" | "large" | "huge";
   icon?: React.ReactNode;
 }
 
-const SpinnerIconOuter = styled('div')(({ theme }) => ({
+const SpinnerIconOuter = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.primary.main,
   fill: theme.palette.primary.contrastText
 }));
 
-const Pulse = styled('div')(({ theme }) => ({
+const Pulse = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.primary.main
 }));
 
-function Spinner({ size = 'medium', icon }: IProps) {
+function Spinner({ size = "medium", icon }: IProps) {
   const className = classNames(
     {
-      'ui-128__spinner--small': size === 'small',
-      'ui-128__spinner--large': size === 'large',
-      'ui-128__spinner--huge': size === 'huge'
+      "ui-128__spinner--small": size === "small",
+      "ui-128__spinner--large": size === "large",
+      "ui-128__spinner--huge": size === "huge"
     },
-    'ui-128__spinner',
-    'ui-128'
+    "ui-128__spinner",
+    "ui-128"
   );
 
   if (!icon) {
-    icon = <Logo className="ui-128__spinner-icon" />;
+    icon = <Router className="ui-128__spinner-icon" />;
   }
 
   return (
     <div className={className}>
       <Pulse className="ui-128__spinner-pulse-outer" />
       <Pulse className="ui-128__spinner-pulse-inner" />
-      <SpinnerIconOuter className="ui-128__spinner-icon-outer">{icon}</SpinnerIconOuter>
+      <SpinnerIconOuter className="ui-128__spinner-icon-outer">
+        {icon}
+      </SpinnerIconOuter>
     </div>
   );
 }
 
 Spinner.propTypes = {
-  size: PropTypes.oneOf(['small', 'medium', 'large', 'huge'])
+  size: PropTypes.oneOf(["small", "medium", "large", "huge"])
 };
 
 Spinner.defaultProps = {
-  size: 'medium'
+  size: "medium"
 };
 
 export default Spinner;
