1cb51caafd6a47940d5b755dfa65f9c91ef54cb0
Benjamin Beshel <bbeshel@128technology.com>
2021-10-25 16:23:26 -0400

    Added functionality for orderBy and orderDirection props to override internal sorting parameters

--- src/components/EnhancedTable/__tests__/EnhancedTable-test.tsx
+++ src/components/EnhancedTable/__tests__/EnhancedTable-test.tsx
@@ -98,7 +98,7 @@ describe('Enhanced Table', () => {
 
     const component = mount(<EnhancedTable data={data} columns={columns} loading={false} height={500} width={500} />);
     const cells = component.find(TableCell);
-    component.find(MuiTable).prop('onHeaderClick')!({ a: 'a' } as any, { column: { name: 'name' } });
+    component.find(MuiTable).prop('onHeaderClick')!({ event: 'fakeEvent' } as any, { column: { name: 'name' } });
     expect(cells.at(1).text()).to.equal('moo moo cow');
     expect(cells.at(2).text()).to.equal('dog');
     expect(cells.at(3).text()).to.equal('cat');
@@ -144,5 +144,50 @@ describe('Enhanced Table', () => {
     expect(cells.at(1).text()).to.equal('4.2.0');
     expect(cells.at(2).text()).to.equal('3.2.0');
     expect(cells.at(3).text()).to.equal('2.2.0');
+  });
+
+  it('should abide by orderBy and orderDirection props', () => {
+    const data = [
+      {
+        name: 'dog',
+        age: 3
+      },
+      {
+        name: 'cat',
+        age: 1
+      },
+      {
+        name: 'moo moo cow',
+        age: 2
+      }
+    ];
+
+    const columns = [
+      {
+        header: 'Name',
+        name: 'name'
+      },
+      {
+        header: 'Age',
+        name: 'age'
+      }
+    ];
+
+    const component = mount(<EnhancedTable data={data} columns={columns} loading={false} height={500} width={500} orderBy="age" orderDirection="asc" />);
+    const cells = component.find(TableCell);
+    expect(cells.at(2).text()).to.equal('cat');
+    expect(cells.at(4).text()).to.equal('moo moo cow');
+    expect(cells.at(6).text()).to.equal('dog');
+    component.find(MuiTable).prop('onHeaderClick')!({ event: 'fakeEvent' } as any, { column: { name: 'name' } });
+    // Expect sort to be UNCHANGED, orderBy exists so it should control the sort and visuals related to it.
+    expect(cells.at(2).text()).to.equal('cat');
+    expect(cells.at(4).text()).to.equal('moo moo cow');
+    expect(cells.at(6).text()).to.equal('dog');
+    const componentTwo = mount(<EnhancedTable data={data} columns={columns} loading={false} height={500} width={500} orderBy="name" orderDirection="desc" />);
+    const cellsTwo = componentTwo.find(TableCell);
+    expect(cellsTwo.at(2).text()).to.equal('moo moo cow');
+    expect(cellsTwo.at(4).text()).to.equal('dog');
+    expect(cellsTwo.at(6).text()).to.equal('cat');
+
   });
 });
